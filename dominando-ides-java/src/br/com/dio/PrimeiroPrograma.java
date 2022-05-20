94e4d10ee83626b5e812c76fbf6a8cba102c1acc
X1K00 <fcju14@gmail.com>
2022-05-20 10:10:02 -0300

    adcionando projeto do curso dominando IDSs Java

---
+++ dominando-ides-java/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+import java.util.Scanner;
+
+import br.com.dio.model.Gato;
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato("Xanin", "Azul", 12);
+		Livro livro = new Livro();
+		
+		/*Scanner input = new Scanner(System.in);
+		System.out.print("Digite um valor para A: ");
+		int a = input.nextInt();
+		System.out.print("Digite um valor para B: ");
+		int b = input.nextInt();
+		System.out.printf("a soma de A + B = %s", a+b);
+		}*/
+	}
+
+class Livro{
+	private String nome;
+	private String npag;
+}
+	
+
+}
